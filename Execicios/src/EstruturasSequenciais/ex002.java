5b7b94b93cdb9306a22371192acda37c05a9cba5
Thiago Silva Bazeredo Salgado <thiagosbsalgado@gmail.com>
2025-04-25 19:36:46 -0300

    ex002-Duarte

---
+++ Execicios/src/EstruturasSequenciais/ex002.java
@@ -0,0 +1,12 @@
+package EstruturasSequenciais;
+
+import java.util.Scanner;
+
+public class ex002 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Escolha um número:");
+        int number = input.nextInt();
+        System.out.println("O número informado foi " + number);
+    }
+}
