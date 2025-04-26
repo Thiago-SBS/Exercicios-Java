415464824ec651089768c68c659b6fe8eba6f9e4
Thiago Silva Bazeredo Salgado <thiagosbsalgado@gmail.com>
2025-04-26 12:56:41 -0300

    att-ex004

--- Execicios/src/EstruturasSequenciais/ex004.java
+++ Execicios/src/EstruturasSequenciais/ex004.java
@@ -7,15 +7,15 @@ public class ex004 {
         Scanner input = new Scanner(System.in);
         System.out.println("Média bimestral\n");
         System.out.println("Primeira nota:");
-        float firstCourseWork = input.nextFloat();
-        System.out.println("Segunda nota:");
-        float secondCourseWork = input.nextFloat();
-        System.out.println("Terceira nota:");
         float firstTestGrade = input.nextFloat();
-        System.out.println("Quarta nota:");
+        System.out.println("Segunda nota:");
         float secondTestGrade = input.nextFloat();
+        System.out.println("Terceira nota:");
+        float thirdTestGrade = input.nextFloat();
+        System.out.println("Quarta nota:");
+        float fourthTestGrade = input.nextFloat();
 
-        float avarage = (firstCourseWork + secondCourseWork + firstTestGrade + secondTestGrade) / 4;
+        float avarage = (firstTestGrade + fourthTestGrade + thirdTestGrade + fourthTestGrade) / 4;
         System.out.printf("A média é %.1f", avarage);
     }
 }
