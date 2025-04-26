fa884db69b8a76eb877a64787bd6a4f2c635bce9
Thiago Silva Bazeredo Salgado <thiagosbsalgado@gmail.com>
2025-04-26 01:20:54 -0300

    ex006-Duarte

---
+++ Execicios/src/EstruturasSequenciais/ex006.java
@@ -0,0 +1,17 @@
+package EstruturasSequenciais;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ex006 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calculadora de área do trianulo.\n");
+        System.out.println("De a área do cilculo:");
+
+        double radius = input.nextDouble();
+        double area = Math.PI * (Math.pow(radius, 2));
+
+        System.out.printf("A área do circulo é %.2fm²", area);
+    }
+}
